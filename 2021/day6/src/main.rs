1bd99bb61a8b147e65ce26dcdb6c923fa68006a2
Lucas Culverhouse <lucasculverhouse@gmail.com>
2023-02-04 09:25:19 -0800

    Clippyify

--- 2021/day6/src/main.rs
+++ 2021/day6/src/main.rs
@@ -6,7 +6,7 @@ fn parse_input(input: &str) -> [u64; 9] {
         .split(',')
         .map(|c| c.parse::<u64>().unwrap())
         .for_each(|num| collection[num as usize] += 1);
-    return collection;
+    collection
 }
 
 fn part_1(fish: &mut [u64; 9]) -> u64 {
@@ -15,7 +15,7 @@ fn part_1(fish: &mut [u64; 9]) -> u64 {
         fish.rotate_left(1);
         fish[6] += born_fish;
     }
-    fish.into_iter().map(|num| *num).sum()
+    fish.iter_mut().map(|num| *num).sum()
 }
 
 fn part_2(fish: &mut [u64; 9]) -> u64 {
@@ -24,15 +24,13 @@ fn part_2(fish: &mut [u64; 9]) -> u64 {
         fish.rotate_left(1);
         fish[6] += born_fish;
     }
-    fish.into_iter().map(|num| *num).sum()
+    fish.iter_mut().map(|num| *num).sum()
 }
 
 fn main() {
     let path = "../inputs/day6.txt";
     let input = fs::read_to_string(path).unwrap();
     let fish = parse_input(&input);
-    let fish_count_part_1 = part_1(&mut fish.clone());
-    let fish_count_part_2 = part_2(&mut fish.clone());
-    println!("Part 1:\n{}", fish_count_part_1);
-    println!("Part 2:\n{}", fish_count_part_2);
+    println!("Part 1:\n{}", part_1(&mut fish.clone()));
+    println!("Part 2:\n{}", part_2(&mut fish.clone()));
 }
