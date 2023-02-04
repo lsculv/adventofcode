923e98f5b6cbc0e95b2ce976ef99aa6fd1e5e1f8
Lucas Culverhouse <lucasculverhouse@gmail.com>
2023-02-04 09:12:56 -0800

    Add 2021 day 6 solution

---
+++ 2021/day6/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn parse_input(input: &str) -> [u64; 9] {
+    let mut collection: [u64; 9] = [0; 9];
+    input
+        .split(',')
+        .filter_map(|c| c.parse::<u64>().ok())
+        .for_each(|num| collection[num as usize] += 1);
+    return collection;
+}
+
+fn part_1(fish: &mut [u64; 9]) -> u64 {
+    println!("{:?}", fish);
+    for _ in 0..80 {
+        let born_fish = fish[0];
+        fish.rotate_left(1);
+        fish[6] += born_fish;
+    }
+    fish.into_iter().map(|num| *num).sum()
+}
+
+fn part_2(fish: &mut [u64; 9]) -> u64 {
+    for _ in 0..256 {
+        let born_fish = fish[0];
+        fish.rotate_left(1);
+        fish[6] += born_fish;
+    }
+    fish.into_iter().map(|num| *num).sum()
+}
+
+fn main() {
+    let path = "../inputs/day6.txt";
+    let input = fs::read_to_string(path).unwrap();
+    let fish = parse_input(&input);
+    let fish_count_part_1 = part_1(&mut fish.clone());
+    let fish_count_part_2 = part_2(&mut fish.clone());
+    println!("Part 1:\n{}", fish_count_part_1);
+    println!("Part 2:\n{}", fish_count_part_2);
+}
