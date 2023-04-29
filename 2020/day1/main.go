67ea7934e5fa94c65f62e5d41a6aeb489a75f048
lsculv <112150142+lsculv@users.noreply.github.com>
2023-04-29 09:09:54 -0700

    Finish day 1 solution

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -38,6 +38,43 @@ func part_1(input string) (int, error) {
 	return -1, errors.New("Could not find two ints that sum to 2020")
 }
 
+func part_2(input string) (int, error) {
+	e := log.New(os.Stderr, "Error: ", 0)
+
+	var num1 int
+	var num2 int
+	var num3 int
+	var err error
+
+	for _, i := range strings.Split(input, "\n") {
+		num1, err = strconv.Atoi(i)
+		if err != nil {
+			e.Println(err)
+		}
+
+		for _, j := range strings.Split(input, "\n") {
+			num2, err = strconv.Atoi(j)
+			if err != nil {
+				e.Println(err)
+			}
+
+			for _, k := range strings.Split(input, "\n") {
+				num3, err = strconv.Atoi(k)
+				if err != nil {
+					e.Println(err)
+				}
+
+				if num1+num2+num3 == 2020 {
+					return num1 * num2 * num3, nil
+				}
+			}
+
+		}
+
+	}
+	return -1, errors.New("Could not find ints that sum to 2020")
+}
+
 func main() {
 	e := log.New(os.Stderr, "Error: ", 0)
 
@@ -59,7 +96,12 @@ func main() {
 	if err != nil {
 		e.Println(err)
 	}
+	part_2_result, err := part_2(input)
+	if err != nil {
+		e.Println(err)
+	}
 
 	fmt.Printf("Part 1: %v\n", part_1_result)
+	fmt.Printf("Part 2: %v\n", part_2_result)
 	return
 }
