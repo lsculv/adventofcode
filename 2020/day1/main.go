65497959ae07242d5703f8ac848e8ae5a95bd34a
lsculv <112150142+lsculv@users.noreply.github.com>
2023-04-29 09:02:58 -0700

    Add 2020 day 1 solution and modify gitignore for including Go in the repo

---
+++ 2020/day1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part_1(input string) (int, error) {
+	e := log.New(os.Stderr, "Error: ", 0)
+
+	var num1 int
+	var num2 int
+	var err error
+
+	for _, i := range strings.Split(input, "\n") {
+		num1, err = strconv.Atoi(i)
+		if err != nil {
+			e.Println(err)
+		}
+
+		for _, j := range strings.Split(input, "\n") {
+			num2, err = strconv.Atoi(j)
+			if err != nil {
+				e.Println(err)
+			}
+
+			if num1+num2 == 2020 {
+				return num1 * num2, nil
+			}
+		}
+
+	}
+	return -1, errors.New("Could not find two ints that sum to 2020")
+}
+
+func main() {
+	e := log.New(os.Stderr, "Error: ", 0)
+
+	f, err := os.Open("../inputs/day1.txt")
+	defer f.Close()
+	if err != nil {
+		e.Println(err)
+	}
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		e.Println(err)
+	}
+
+	input := string(bytes)
+	input = strings.TrimSuffix(input, "\n")
+
+	part_1_result, err := part_1(input)
+	if err != nil {
+		e.Println(err)
+	}
+
+	fmt.Printf("Part 1: %v\n", part_1_result)
+	return
+}
