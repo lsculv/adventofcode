aa79779f95e8bc1e9899ac9499dd8596a5dd9e89
Lucas Culverhouse <lucasculverhouse@gmail.com>
2023-12-01 12:59:25 -0800

    2023: day 1 full solution

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,126 @@
+fn main() {
+    let input: &[u8] = include_bytes!("../day01.txt");
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+fn part_1(input: &[u8]) -> u32 {
+    let mut sum = 0;
+    for line in input.split(|&b| b == b'\n') {
+        let tens = line
+            .iter()
+            .find(|b| b.is_ascii_digit())
+            .expect("line should always contain at least one digit")
+            - b'0';
+        let ones = line
+            .iter()
+            .rev()
+            .find(|b| b.is_ascii_digit())
+            .expect("line should always contain at least one digit")
+            - b'0';
+        sum += (tens * 10 + ones) as u32;
+    }
+
+    sum
+}
+
+fn part_2(input: &[u8]) -> u32 {
+    let mut sum = 0;
+    for line in input.split(|&n| n == b'\n') {
+        let tens = find_digit_left(line);
+        let ones = find_digit_right(line);
+        sum += tens * 10 + ones;
+    }
+
+    sum
+}
+
+fn find_digit_right(line: &[u8]) -> u32 {
+    for i in (0..line.len()).rev() {
+        if line[i].is_ascii_digit() {
+            return (line[i] - b'0') as u32;
+        }
+        match line[i] {
+            b'e' => match line[i - 1] {
+                b'n' => match line[i - 2] {
+                    b'o' => return 1,
+                    b'i' if line[i - 3] == b'n' => return 9,
+                    _ => continue,
+                },
+                b'v' if checkr(line, i - 2, &[b'f', b'i']) => return 5,
+                b'e' if checkr(line, i - 2, &[b't', b'h', b'r']) => return 3,
+                _ => continue,
+            },
+            b'o' if checkr(line, i - 1, &[b't', b'w']) => return 2,
+            b'r' if checkr(line, i - 1, &[b'f', b'o', b'u']) => return 4,
+            b'x' if checkr(line, i - 1, &[b's', b'i']) => return 6,
+            b'n' if checkr(line, i - 1, &[b's', b'e', b'v', b'e']) => return 7,
+            b't' if checkr(line, i - 1, &[b'e', b'i', b'g', b'h']) => return 8,
+            _ => continue,
+        }
+    }
+    unreachable!("Must reach a digit.");
+}
+
+fn find_digit_left(line: &[u8]) -> u32 {
+    for i in 0..line.len() {
+        if line[i].is_ascii_digit() {
+            return (line[i] - b'0') as u32;
+        }
+        match line[i] {
+            b'o' if checkl(line, i + 1, &[b'n', b'e']) => return 1,
+            b't' => match line[i + 1] {
+                b'w' if line[i + 2] == b'o' => return 2,
+                b'h' if checkl(line, i + 2, &[b'r', b'e', b'e']) => return 3,
+                _ => continue,
+            },
+            b'f' => match line[i + 1] {
+                b'o' if checkl(line, i + 2, &[b'u', b'r']) => return 4,
+                b'i' if checkl(line, i + 2, &[b'v', b'e']) => return 5,
+                _ => continue,
+            },
+            b's' => match line[i + 1] {
+                b'i' if line[i + 2] == b'x' => return 6,
+                b'e' if checkl(line, i + 2, &[b'v', b'e', b'n']) => return 7,
+                _ => continue,
+            },
+            b'e' if checkl(line, i + 1, &[b'i', b'g', b'h', b't']) => return 8,
+            b'n' if checkl(line, i + 1, &[b'i', b'n', b'e']) => return 9,
+            _ => continue,
+        }
+    }
+    unreachable!("Must reach a digit.");
+}
+
+fn checkl(slice: &[u8], start: usize, cmp: &[u8]) -> bool {
+    for i in 0..cmp.len() {
+        if slice[start + i] != cmp[i] {
+            return false;
+        }
+    }
+    true
+}
+
+fn checkr(slice: &[u8], end: usize, cmp: &[u8]) -> bool {
+    for i in 0..cmp.len() {
+        if slice[end - i] != cmp[cmp.len() - (i + 1)] {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_part_1() {
+        let input: &[u8] = b"1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n";
+        assert_eq!(part_1(input), 142);
+    }
+    #[test]
+    fn test_part_2() {
+        let input: &[u8] = b"two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n";
+        assert_eq!(part_2(input), 142);
+    }
+}
